a75d4feff329f03cbafe8d488e03a82e94577cbb
Marcos Iscovich <miscovich@na-sa.com.ar>
2023-03-01 11:38:00 -0300

    pwaOFFLINE

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,116 @@
+<script setup>
+import { defineComponent, ref, onMounted } from "vue";
+import { useQuasar } from "quasar";
+
+const systemOnline = ref(false);
+
+const $q = useQuasar();
+const menuDrawer = ref(false);
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Ir a central Nuclear Embalse",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://www.na-sa.com.ar/",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+const openPage = () => {
+  $q.dialog({
+    title: "Funciona el boton",
+    message: "Este boton si funciona",
+  });
+};
+
+onMounted(() => {
+  if (navigator.onLine) {
+    console.log("Conectado a internet");
+    systemOnline.value = true;
+  }
+  if (!navigator.onLine) {
+    console.log("Sin conexión a internet");
+    systemOnline.value = false;
+  }
+});
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <div class="q-mt-md row">
+          <q-fab
+            v-model="menuDrawer"
+            label="Actions"
+            label-position="bottom"
+            vertical-actions-align="left"
+            color="white"
+            icon="keyboard_arrow_down"
+            direction="down"
+            flat
+          >
+            <q-fab-action color="primary" @click="openPage" icon="mail" label="Nueva Rutina" />
+            <q-fab-action color="secondary" @click="openPage" icon="alarm" label="Editar Variable" />
+            <q-fab-action color="orange" @click="openPage" icon="airplay" label="Buscar Rutina" />
+            <q-fab-action color="accent" @click="openPage" icon="room" label="Nueva Medición" />
+          </q-fab>
+        </div>
+
+        <q-toolbar-title> Nuevo sistema de Mediciones </q-toolbar-title>
+
+        <div>
+          <q-icon :name="systemOnline ? 'wifi' : 'mdi-wifi-off'" :color="systemOnline ? 'secondary' : 'negative'" size="4rem"/> 
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <!-- <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Links de redireccionamiento </q-item-label>
+
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer> -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,251 @@
+<script setup>
+import { date, useQuasar } from "quasar";
+import { ref, onMounted } from "vue";
+import Swal from "sweetalert2";
+
+const $q = useQuasar();
+
+/* const variables = [
+  {
+    title: "variable 1",
+    icon: "mdi-account-convert",
+    value: "0",
+  },
+  {
+    title: "VARIABLE 2",
+    icon: "mdi-account-cowboy-hat",
+    value: "0",
+  },
+  {
+    title: "variable 3",
+    icon: "mdi-air-horn",
+    value: "0",
+  },
+  {
+    title: "VARIABLE 4",
+    icon: "mdi-alarm-bell",
+    value: "0",
+  },
+  {
+    title: "variable 5",
+    icon: "mdi-alert-octagon",
+    value: "0",
+  },
+  {
+    title: "VARIABLE 6",
+    icon: "mdi-alien",
+    value: "0",
+  },
+  {
+    title: "variable 7",
+    icon: "mdi-alien-outline",
+    value: "0",
+  },
+  {
+    title: "VARIABLE 8",
+    icon: "mdi-alien-outline",
+    value: "0",
+  },
+  {
+    title: "variable 9",
+    icon: "mdi-alien",
+    value: "0",
+  },
+  {
+    title: "VARIABLE 10",
+    icon: "mdi-alien-outline",
+    value: "0",
+  },
+  {
+    title: "variable 11",
+    icon: "mdi-alien",
+    value: "0",
+  },
+  {
+    title: "VARIABLE 12",
+    icon: "mdi-alien-outline",
+    value: "0",
+  },
+  {
+    title: "variable 13",
+    icon: "mdi-alien",
+    value: "0",
+  },
+  {
+    title: "variable 14",
+    icon: "mdi-alien-outline",
+    value: "0",
+  },
+  {
+    title: "variable 15",
+    icon: "mdi-alien",
+    value: "0",
+  },
+  {
+    title: "variable 16",
+    icon: "mdi-alien-outline",
+    value: "0",
+  },
+  {
+    title: "variable 17",
+    icon: "mdi-alien",
+    value: "0",
+  },
+  {
+    title: "variable 18",
+    icon: "mdi-alien-outline",
+    value: "0",
+  },
+  {
+    title: "variable 19",
+    icon: "mdi-alien",
+    value: "0",
+  },
+  {
+    title: "variable 20",
+    icon: "mdi-alien-outline",
+    value: "0",
+  },
+  {
+    title: "variable 21",
+    icon: "mdi-alien",
+    value: "0",
+  },
+  {
+    title: "variable 22",
+    icon: "mdi-alien-outline",
+    value: "0",
+  },
+]; */
+
+const variables = ref([
+  { value: null },
+  { value: null },
+  { value: null },
+  { value: null },
+  { value: null },
+  { value: null },
+  { value: null },
+  { value: null },
+  { value: null },
+  { value: null },
+]);
+
+const submitForm = () => {
+  console.log(variables.value);
+
+  objectStore.add({
+    valor: variables.value,
+    fecha: date.formatDate(new Date(), "YYYY-MM-DD HH:mm:ss"),
+  });
+
+  /*  variables.value = [
+    { valor: null },
+    { valor: null },
+    { valor: null },
+    { valor: null },
+    { valor: null },
+    { valor: null },
+    { valor: null },
+    { valor: null },
+    { valor: null },
+    { valor: null },
+  ]; */
+};
+
+const openVar = () => {
+  Swal.fire({
+    title: "Variables",
+    html: `
+    ${variables.value
+      .map((variable, index) => {
+        return `
+        <div class="q-pa-md" style="max-width: 350px">
+        <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label> varaible ${index + 1}</q-item-label>
+          <q-item-label caption>valor = ${variable.valor}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </div>
+    `;
+      })
+      .join("")}
+    `,
+  });
+};
+
+onMounted(() => {
+  let request = indexedDB.open("variables2", 1);
+  request.onsuccess = function (event) {
+    let db = event.target.result;
+  };
+
+  request.onupgradeneeded = function (event) {
+    let db = event.target.result;
+    var objectStore = db.createObjectStore("datosVariables", {
+      keyPath: "id",
+      autoIncrement: true,
+    });
+    objectStore.createIndex("valor", "valor", { unique: false });
+    objectStore.createIndex("fecha", "fecha", { unique: false });
+    objectStore.add({
+      valor: variables.value,
+      fecha: date.formatDate(new Date(), "YYYY-MM-DD HH:mm:ss"),
+    });
+  };
+
+  navigator.onLine &&
+    $q.notify({
+      message: "Conectado a internet",
+      color: "positive",
+      icon: "mdi-check",
+    });
+});
+</script>
+
+<template>
+  <q-responsive :ratio="1" class="row">
+    <div class="col">
+      <div class="flex justify-between">
+        <q-btn @click="openVar" color="primary" label="Ver datos" />
+        <q-btn @click="openVar" color="secondary" label="Recuperar Datos" />
+      </div>
+
+      <q-form ref="form" @submit="submitForm">
+        <q-card>
+          <q-card-section>
+            <q-input v-for="(variable, index) in variables" :key="index" :label="'Variable ' + (index + 1)" v-model="variable.valor" type="number" />
+          </q-card-section>
+          <q-card-actions>
+            <q-btn type="submit" color="primary" label="Guardar" />
+            <q-btn type="reset" color="negative" label="Limpiar" />
+          </q-card-actions>
+        </q-card>
+      </q-form>
+    </div>
+
+    <!-- <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+        <q-input filled v-model="name" label="Usuario" hint="ingrese su usuario" lazy-rules />
+        <q-input filled v-model="mediciones.variable2" label="variable 1"> </q-input>
+        <q-input filled v-model="mediciones.variable3" label="variable 2"> </q-input>
+        <q-input filled v-model="mediciones.variable4" label="variable 3"> </q-input>
+        <q-input filled v-model="mediciones.variable5" label="variable 4"> </q-input>
+        <q-input filled v-model="mediciones.variable6" label="variable 5"> </q-input>
+        <q-input filled v-model="mediciones.variable7" label="variable 6"> </q-input>
+
+
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </q-form> -->
+  </q-responsive>
+</template>
+
+<style scoped>
+.cardClass {
+  width: 800px;
+}
+</style>
